072a128a37c83979f2206a96ec9675eee703b532
Daniel <daniel@fasttrack-solutions.com>
2022-03-29 09:36:38 +0200

    WIP: Debugging

--- clickhouse_mysql/reader/mysqlreader.py
+++ clickhouse_mysql/reader/mysqlreader.py
@@ -312,68 +312,68 @@ class MySQLReader(Reader):
 
         # fetch events
         # try:
-            while True:
-                logging.debug('Check events in binlog stream')
+        while True:
+            logging.debug('Check events in binlog stream')
 
-                self.init_fetch_loop()
+            self.init_fetch_loop()
 
-                # statistics
-                self.stat_init_fetch_loop()
+            # statistics
+            self.stat_init_fetch_loop()
 
-                try:
-                    logging.debug('Pre-start binlog position: ' + self.binlog_stream.log_file + ":" + str(self.binlog_stream.log_pos) if self.binlog_stream.log_pos is not None else "undef")
+            try:
+                logging.debug('Pre-start binlog position: ' + self.binlog_stream.log_file + ":" + str(self.binlog_stream.log_pos) if self.binlog_stream.log_pos is not None else "undef")
 
-                    # fetch available events from MySQL
-                    for mysql_event in self.binlog_stream:
-                        # new event has come
-                        # check what to do with it
+                # fetch available events from MySQL
+                for mysql_event in self.binlog_stream:
+                    # new event has come
+                    # check what to do with it
 
-                        logging.debug('Got Event ' + self.binlog_stream.log_file + ":" + str(self.binlog_stream.log_pos))
+                    logging.debug('Got Event ' + self.binlog_stream.log_file + ":" + str(self.binlog_stream.log_pos))
 
-                        # process event based on its type
-                        if isinstance(mysql_event, WriteRowsEvent):
-                            self.process_write_rows_event(mysql_event)
-                        elif isinstance(mysql_event, DeleteRowsEvent):
-                            self.process_delete_rows_event(mysql_event)
-                        elif isinstance(mysql_event, UpdateRowsEvent):
-                            self.process_update_rows_event(mysql_event)
-                        else:
-                            # skip other unhandled events
-                            pass
-
-                        # after event processed, we need to handle current binlog position
-                        self.process_binlog_position(self.binlog_stream.log_file, self.binlog_stream.log_pos)
-
-                except KeyboardInterrupt:
-                    # pass SIGINT further
-                    logging.info("SIGINT received. Pass it further.")
-                    raise
-                except Exception as ex:
-                    if self.blocking:
-                        # we'd like to continue waiting for data
-                        # report and continue cycle
-                        logging.warning("Got an exception, skip it in blocking mode")
-                        logging.warning(ex)
+                    # process event based on its type
+                    if isinstance(mysql_event, WriteRowsEvent):
+                        self.process_write_rows_event(mysql_event)
+                    elif isinstance(mysql_event, DeleteRowsEvent):
+                        self.process_delete_rows_event(mysql_event)
+                    elif isinstance(mysql_event, UpdateRowsEvent):
+                        self.process_update_rows_event(mysql_event)
                     else:
-                        # do not continue, report error and exit
-                        logging.critical("Got an exception, abort it in non-blocking mode")
-                        logging.critical(ex)
-                        sys.exit(1)
+                        # skip other unhandled events
+                        pass
 
-                # all events fetched (or none of them available)
+                    # after event processed, we need to handle current binlog position
+                    self.process_binlog_position(self.binlog_stream.log_file, self.binlog_stream.log_pos)
 
-                # statistics
-                self.stat_close_fetch_loop()
+            except KeyboardInterrupt:
+                # pass SIGINT further
+                logging.info("SIGINT received. Pass it further.")
+                raise
+            except Exception as ex:
+                if self.blocking:
+                    # we'd like to continue waiting for data
+                    # report and continue cycle
+                    logging.warning("Got an exception, skip it in blocking mode")
+                    logging.warning(ex)
+                else:
+                    # do not continue, report error and exit
+                    logging.critical("Got an exception, abort it in non-blocking mode")
+                    logging.critical(ex)
+                    sys.exit(1)
 
-                if not self.blocking:
-                    # do not wait for more data - all done
-                    break  # while True
+            # all events fetched (or none of them available)
 
-                # blocking - wait for more data
-                if self.nice_pause > 0:
-                    time.sleep(self.nice_pause)
+            # statistics
+            self.stat_close_fetch_loop()
 
-                self.notify('ReaderIdleEvent')
+            if not self.blocking:
+                # do not wait for more data - all done
+                break  # while True
+
+            # blocking - wait for more data
+            if self.nice_pause > 0:
+                time.sleep(self.nice_pause)
+
+            self.notify('ReaderIdleEvent')
 
         # except KeyboardInterrupt:
         #     logging.info("SIGINT received. Time to exit.")
