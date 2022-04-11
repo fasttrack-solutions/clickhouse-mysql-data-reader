31d98acb23072c1ceaab21f18a83d439bedca92f
Curt Healey <curthealey@ip-192-168-1-64.eu-west-1.compute.internal>
2022-04-11 23:36:42 +0200

    testing changes

--- clickhouse_mysql/reader/mysqlreader.py
+++ clickhouse_mysql/reader/mysqlreader.py
@@ -356,7 +356,6 @@ class MySQLReader(Reader):
                     if self.fatal_on_exception:
                         # we'd like to continue waiting for data
                         # report and continue cycle
-                        logging.info("this is the current status of the new flag added", self.fatal_on_exception)
                         logging.critical("Got an exception, abort in fataling mode")
                         logging.critical(ex)
                         sys.exit(1)
